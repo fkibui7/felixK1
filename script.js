2f5d6d7bfbc20933789100b06d695c231dc86ec9
kibui-ent <kafuefelix@gmail.com>
2024-03-27 14:36:04 +0300

    toggle

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
- function toggleMenu(){
-    const menu = document.queryselector(".menu-links");
-    const icon = document.queryselector(".hamburger-icon");
-    menu.classlist.toggle("open")
-    icon.classlist.toggle("open")
- }
+function toggleMenu(params) {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+}
